b264910cddf5f5525bbc32499204fe385de099f2
tetakashi <83709530+tetakashi@users.noreply.github.com>
2021-05-10 10:36:56 -0700

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt install git -y
-git clone https://github.com/tetakashi/try-xmrig
-cd try-xmrig
+git clone https://github.com/tetakashi/dev1-xmrig
+cd dev1-xmrig
 chmod u+x xmrig
 screen -S xmrig
